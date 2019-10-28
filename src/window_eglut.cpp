789f14201b7a4a184c66a2d6863d4550f641c6d5
Christopher Homberger <christopher.homberger@web.de>
2019-10-29 00:38:50 +0100

    Poll Window Events eglut

--- src/window_eglut.cpp
+++ src/window_eglut.cpp
@@ -77,6 +77,7 @@ void EGLUTWindow::setFullscreen(bool fullscreen) {
 
 void EGLUTWindow::swapBuffers() {
     eglutSwapBuffers();
+    eglutPollEvents();
 }
 
 void EGLUTWindow::swapInterval(int interval) {
